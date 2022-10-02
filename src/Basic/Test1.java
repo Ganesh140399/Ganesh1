ee4667ab89f14d11b28ca6cdbeea48f529e38c90
Lenovo <Lenovo@LAPTOP-91EJ1UJ3>
2022-10-02 12:52:01 +0530

    First Commit

---
+++ src/Basic/Test1.java
@@ -0,0 +1,19 @@
+package Basic;
+
+public class Test1 
+{
+  public static void main(String[] args) 
+  {
+	  System.out.println("My name is Ganesh Patil");
+	  m1();
+	  m2();
+  }	
+  public static void m1() 
+  {
+	  System.out.println("My name is Nishan Patil");
+  }
+  public static void m2() 
+  {
+	  System.out.println("My name is Mrunal Patil");
+  }
+}
